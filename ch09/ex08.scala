18f111a7cbeb317aca53737396a848c6499039f2
Saleem Ansari <tuxdna@gmail.com>
2013-03-04 16:12:16 +0530

    Search img ulr using regex

--- ch09/ex08.scala
+++ ch09/ex08.scala
@@ -3,5 +3,18 @@
  * tags of a web page. Use regular expressions and groups.
  */
 
+import scala.io.Source
 
-// TODO: Incomplete.
+object ex08 extends App {
+  var url = ""
+  if (args.length < 1) {
+    println("No input from the command line. Exiting.")
+    sys.exit(-1)
+  }
+  url = args(0)
+  var source = Source.fromURL(url, "UTF-8")
+  val imgRegex = """<img src=\"(.*?)\".*/>""".r
+  for( m <- imgRegex.findAllIn(source.mkString).matchData) {
+    println("Image URL: %s".format(m.group(1)))
+  }
+}
