b3c3c714ec8d510f793bdf4a8dcf75becb2471bd
Saleem Ansari <tuxdna@gmail.com>
2013-03-22 17:07:03 +0530

    time.scala: function as a parameter

---
+++ various/timer.scala
@@ -0,0 +1,15 @@
+
+object Timer {
+  def oncePerSecond(callback: () => Unit) {
+    while(true) { callback(); Thread sleep 1000 }
+  }
+
+  def timeFlies() {
+    println("time flies like an arrow...")
+  }
+
+  def main(args: Array[String]) {
+    oncePerSecond(timeFlies)
+  }
+}
+
