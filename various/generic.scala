9ad0b7dec5cee396b98acd89592787ef971105c5
Saleem Ansari <tuxdna@gmail.com>
2013-03-22 20:16:20 +0530

    generic in scala example

--- various/generic.scala
+++ various/generic.scala
@@ -1,9 +1,24 @@
+object Generics {
 
-type UnaryOp[T, R] = T => R
+  type UnaryOp[T, R] = T => R
+  def run[R]( gen: UnaryOp[Int,R] ) {
+    for(x <- 1 to 5) println(gen(x))
+  }
 
-def run[R]( gen: UnaryOp[Int,R] ) {
-  for(x <- 1 to 5) println(gen(x))
+  class Reference[T] {
+    private var contents: T = _
+    def set(value: T) { contents = value }
+    def get: T = contents
+  }
+
+
+  def main(args: Array[String]) {
+    run( x => -x )
+    run( x => Array.fill(x)("*").mkString )
+
+    val cell = new Reference[Int]
+    cell.set(13)
+    println("Reference contains the half of " + (cell.get * 2 ))
+  }
+
 }
-
-run( x => -x )
-run( x => Array.fill(x)("*").mkString )
