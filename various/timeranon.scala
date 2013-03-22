458ba1a31df7e04810444648467b2192c24b6bcc
Saleem Ansari <tuxdna@gmail.com>
2013-03-22 17:09:10 +0530

    time.scala: anonymous function

---
+++ various/timeranon.scala
@@ -0,0 +1,11 @@
+object TimerAnonymous {
+  def oncePerSecond(callback: () => Unit) {
+    while(true) { callback(); Thread sleep 1000 }
+  }
+
+  def main(args: Array[String]) {
+    oncePerSecond(  () =>
+	println("time flies like an arrow...")
+      )
+  }
+}
