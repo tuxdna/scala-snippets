959ea2fab7e9f62245d00d26307d9bd1980edd38
Saleem Ansari <tuxdna@gmail.com>
2013-08-28 00:06:01 +0530

    comment out to fix type erasure error

--- src/main/scala/various/type-erasure-wont-compile.scala
+++ src/main/scala/various/type-erasure-wont-compile.scala
@@ -1,4 +1,10 @@
+package various.typeerasure
+
 object Foo {
   def bar(list: List[String]) = list.toString
-  def bar(list: List[Int]) = list.size.toString
+
+  // uncomment following line wont compile
+  
+  //def bar(list: List[Int]) = list.size.toString
 }
+
