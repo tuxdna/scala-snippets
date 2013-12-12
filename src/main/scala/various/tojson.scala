2b2c207a5d6c66c141a7cae6edc53f5a7a2a508e
Saleem Ansari <tuxdna@gmail.com>
2013-12-12 22:16:24 +0530

    tojson

---
+++ src/main/scala/various/tojson.scala
@@ -0,0 +1,42 @@
+package various
+
+object tojson extends App {
+def toJson(a: Any): String = {
+    a match {
+      // number
+      case m: Number => m.toString
+      
+      // string
+      case m: String => "\"" + m + "\""
+      
+      // map
+      case m: Map[AnyRef, AnyRef] => {
+        "{" + (m map { x => val key = x._1; toJson(key) + ": " + toJson(m(key)) } mkString (", ")) + "}"
+      }
+
+      // list
+      case l: Seq[AnyRef] => { "[" + (l map (toJson(_)) mkString (",")) + "]" }
+
+      // for anything else: tuple
+      case m: Product => toJson(m.productIterator.toList)
+      case m: AnyRef => "\"" + m.toString + "\""
+    }
+  }
+
+  class Person(val name: String, val country: String) {
+    override def toString = s"Person(Name: $name, Country: $country)"
+  }
+  val s = "Hello"
+  val i = 120
+  val l = List(1, 2, 3)
+  val m = Map("A" -> "230", "B" -> 2)
+  val ml = Map("A" -> l, "B" -> l)
+  val tuple = (i, l, m, ml, new Person("Bond", "Italy"))
+
+  println(toJson(s))
+  println(toJson(i))
+  println(toJson(l))
+  println(toJson(m))
+  println(toJson(ml))
+  println(toJson(tuple))
+}
