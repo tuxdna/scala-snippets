004c351d37cf66babaefe9100729552e62c9e939
Saleem Ansari <tuxdna@gmail.com>
2014-01-14 08:58:01 +0530

    unapply fixed

--- src/main/scala/ch11/ex10.scala
+++ src/main/scala/ch11/ex10.scala
@@ -1,31 +1,50 @@
 package ch11
 
+import java.io.File
 
 // TODO: In progress
 object ex10 extends App {
-  class RichFile(val f: String) {
+  class RichFile(filePath: String) {
+    val f = new File(filePath)
     def unapply() = {
-      val c = f.split("/")
-      val x = c.last.split(".")
-
+      val basename = f.getName()
+      val dirname = f.getParent()
+      val ei = basename.indexOf(".")
+      if (ei >= 0) {
+        Some((dirname, basename.substring(0, ei), basename.substring(ei + 1)))
+      } else {
+        Some((dirname, basename, ""))
+      }
     }
-    def unapplySeq(): Seq[String] = {
-      f.split("/")
+    def unapplySeq(): Option[Seq[String]] = {
+      val filePath = f.getAbsolutePath()
+      if (filePath.length() == 0)
+        None
+      else
+        Some(filePath.split("/"))
     }
   }
 
   object RichFile {
+    def apply(filePath: String) = new RichFile(filePath)
     def unapply(f: RichFile) = {
       f.unapply
     }
-    def unapplySeq(f: RichFile): Seq[String] = {
+    def unapplySeq(f: RichFile) = {
       f.unapplySeq()
     }
   }
 
-  val f1 = new RichFile("/abc/def/name.txt")
-  //  f1 match {
-  //    case RichFile(a, b, c) =>
-  //  }
+  val l = List(
+    RichFile("/abc/def/name.txt"),
+    RichFile("/home/cay/name.txt"),
+    RichFile("/a/b/c/d/e"))
+
+  l.foreach { f =>
+    f match {
+      // case x :: y => println((x,y))
+      case RichFile(a, b, c) => println((a, b, c))
+    }
+  }
 
 }
