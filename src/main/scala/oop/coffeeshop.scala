3e02822a52b6feb7b3570c8e711bec3e8c0e0de8
Saleem Ansari <tuxdna@gmail.com>
2014-02-24 17:51:14 +0530

    coffee shop

--- src/main/scala/oop/coffeeshop.scala
+++ src/main/scala/oop/coffeeshop.scala
@@ -1,9 +1,22 @@
 package oop
 
 object coffeeshop {
-  class Coffee(val price: Int) { def this() = this(10) }
-  class CreditCard
-  class Payments { def charge(cc: CreditCard, price: Int) {} }
+  class Coffee(val price: Double) {
+    def this() = this(11.5)
+    override def toString() = "Coffee(%.2f)".format(price)
+  }
+
+  case class CreditCard(name: String)
+
+  class Payments { def charge(cc: CreditCard, price: Double) {} }
+
+  case class Charge(cc: CreditCard, amount: Double) {
+    def combine(other: Charge) = {
+      if (cc == other.cc) {
+        Charge(cc, amount + other.amount)
+      } else throw new Exception("Can't combine amount from different cards")
+    }
+  }
 
   class Cafe {
     def buyCoffee(cc: CreditCard, p: Payments): Coffee = {
@@ -11,11 +24,30 @@ object coffeeshop {
       p.charge(cc, cup.price)
       cup
     }
+
+    def buyCoffee(cc: CreditCard) = {
+      val cup = new Coffee()
+      (cup, Charge(cc, cup.price))
+    }
+
+    def buyCoffees(cc: CreditCard, n: Int) = {
+      val purchases = List.fill(n)(buyCoffee(cc))
+      val (coffees, charges) = purchases.unzip
+      (coffees, charges.reduce((a, b) => a.combine(b)))
+    }
   }
+
+  def coalesce(charges: List[Charge]) = {
+    charges.groupBy(_.cc).map(e => (e._1, e._2.reduce((a, b) => a.combine(b))))
+  }
+
   def main(args: Array[String]) {
-    val cc = new CreditCard
+    val cc = CreditCard("Indo-Bank")
     val p = new Payments
     val coffeeShop = new Cafe
     val cup = coffeeShop.buyCoffee(cc, p)
+    println(cup)
+    val cups = coffeeShop.buyCoffees(cc, 10)
+    println(cups)
   }
 }
